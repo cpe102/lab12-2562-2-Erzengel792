dccfe7921e0bb8e2aa9c41a50fb8fc8f4e0bcfc6
Erzengel792 <tanadol_boat@hotmail.com>
2020-02-07 16:10:55 +0700

    Edited V.1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -18,3 +18,21 @@ int main()
 }
 
 //Write definition of stat() here 
+void stat(const double A[],int B,double C[]){
+double zigma = 0;
+for (int i = 0;i<B;i++){
+C[0] += A[i];
+     }
+   C[0] = C[0]/B;
+for(int z = 0;z<B;z++){ 
+zigma += pow(A[z],2);
+} 
+C[1]= sqrt(1/(float)B*zigma-pow(C[0],2));
+for(int count = 0;count < B;count++){
+if(A[count]>C[2]) C[2] = A[count];
+if(A[count]<C[3]) C[3] = A[count];
+}
+
+}
+
+
--- lab12_2.cpp
+++ lab12_2.cpp
@@ -22,4 +22,32 @@ int main()
              return 0;    
 }
 
-// Write definition of updateImage() and showImage() here
+
+void updateImage(bool image[][M],int s,int x,int y){
+    
+    for(int i = 0; i<N ; i++){
+        for(int j = 0;j<M;j++){
+            if(sqrt(pow(i-x,2)+pow(j-y,2)) <= s-1)image[i][j] = true;
+        }
+    }
+}
+
+void showImage(const bool image[][M]){
+
+    for(int i = 0;i<M+2;i++){
+        cout<<"-";
+    }
+    cout<<"\n";
+    for(int i = 0;i<N;i++){
+        cout<< "|";
+      for(int j = 0;j<M;j++){  
+        if(image[i][j] == false) cout << " ";
+        else cout<< "*";
+      }
+      cout << "|"<<"\n";
+    }
+    for(int i = 0;i<M+2;i++){
+        cout<<"-";
+    }
+    cout<<"\n";
+}
